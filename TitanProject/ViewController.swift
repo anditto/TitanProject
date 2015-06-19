886aa445b676c8b0cff368a5a5be419c2cbd7460
Risa Hiyama <risa.hiyama@gmail.com>
2015-06-19 17:25:15 +0900

    Initial Commit

---
+++ TitanProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TitanProject
+//
+//  Created by 樋山理紗 on 2015/06/19.
+//  Copyright (c) 2015年 rhiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
